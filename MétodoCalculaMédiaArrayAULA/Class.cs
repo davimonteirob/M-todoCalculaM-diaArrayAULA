db492fdbc69b546cf3b151dc79b0b16663af3f4c
Davi Monteiro <davi321522@gmail.com>
2024-06-17 09:04:59 -0300

    study array

---
+++ MétodoCalculaMédiaArrayAULA/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MétodoCalculaMédiaArrayAULA
+{
+    internal class Class
+    {
+   
+        
+        public double CalcularMédia(double[] array01) 
+        { 
+            Array.Sort(array01);
+            double soma = 0;
+            for (int posi = 0; posi < array01.Length; posi++) 
+            {
+                soma += array01[posi];
+            
+            }
+            double media = soma/array01.Length;
+            return media;
+
+        
+        }
+    }
+}
---
+++ MétodoCalculaMédiaArrayAULA/Program.cs
@@ -0,0 +1,12 @@
+﻿//Contrataram você como o novo desenvolvedor da equipe da Esther,
+//e você ficou responsável de iniciar a codificação do módulo de estatística da aplicação,
+//iniciando o desenvolvimento da função que irá calcular a média de uma amostra.
+//Essa amostra será um vetor de doubles que será o parâmetro de entrada do seu método.
+
+using MétodoCalculaMédiaArrayAULA;
+
+double[] amostra = new double[] { 10.5, 30.5 ,50 ,75.3 ,103 };
+Class classA = new Class();
+
+Console.WriteLine("A média da amostra é: ");
+classA.CalcularMédia(amostra);
